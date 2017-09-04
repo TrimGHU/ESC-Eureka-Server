ca9a3d8fa95ecc745f083a78c672705361de5564
TrimGHU <530412087@qq.com>
2017-09-05 01:24:11 +0800

    init eureka server project

---
+++ src/main/java/com/hugui/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.hugui.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+@date : 2017年8月8日
+@author : hugui
+@description : TODO
+**/
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
